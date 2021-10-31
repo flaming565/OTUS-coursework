6e4305e282d073f28eb35b9c82c4a7fe60d2eaac
Malkina Anna <amalkina@xdsoft.ru>
2021-10-31 09:22:19 +0300

    Обновила отображение цитаты

--- app/src/main/java/com/amalkina/beautydiary/ui/home/models/QuoteModel.kt
+++ app/src/main/java/com/amalkina/beautydiary/ui/home/models/QuoteModel.kt
@@ -4,25 +4,26 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.map
 import com.amalkina.beautydiary.ui.common.models.BaseModel
 
-const val MAX_COLLAPSE_QUOTE_SIZE = 60
+const val MAX_COLLAPSE_QUOTE_SIZE = 100
 
 internal open class QuoteModel(
     val quote: String = "",
     var author: String = ""
 ) : BaseModel() {
 
+    val isAuthorShown = author.isNotEmpty()
+
     init {
-        if (author.isNotEmpty())
-            author = "— $author"
+        if (isAuthorShown) author = "— $author"
     }
 
     private val isCardCollapse = MutableLiveData(true)
-    val isAuthorShown = isCardCollapse.map { !it && author.isNotEmpty() }
     val quoteText = isCardCollapse.map {
         if (it && quote.length > MAX_COLLAPSE_QUOTE_SIZE)
             "${quote.substring(0, MAX_COLLAPSE_QUOTE_SIZE).trim()}..."
         else quote
     }
+    val maxLines = isCardCollapse.map { if (it) 2 else 10 }
 
     fun changeCardCollapseMode() {
         isCardCollapse.value?.let {
